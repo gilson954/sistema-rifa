a920e57d0c292646b07ea26d1258f04a4839803c
gilson954 <gilsonguigui0@gmail.com>
2025-06-29 16:41:21 -0300

    Ajustar layout do dashboard para largura controlada

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -10,7 +10,7 @@ const DashboardLayout = () => {
       <div className="flex-1 flex flex-col min-w-0">
         <DashboardHeader />
         <div className="flex-1 overflow-y-auto p-4 sm:p-6 text-gray-900 dark:text-white">
-          <div className="w-full max-w-none">
+          <div className="w-full max-w-5xl mx-auto">
             <Outlet />
           </div>
         </div>
