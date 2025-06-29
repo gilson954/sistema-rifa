fc76edaadeed763394c1ab750c44979f0196cdde
gilson954 <gilsonguigui0@gmail.com>
2025-06-29 03:51:56 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,6 @@ import Layout from './components/Layout';
 import DashboardLayout from './components/DashboardLayout';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
-import CampaignsPage from './pages/CampaignsPage';
-import ConfigurePixPage from './pages/ConfigurePixPage';
-import SocialMediaPage from './pages/SocialMediaPage';
-import AnalyticsPage from './pages/AnalyticsPage';
-import CustomizePage from './pages/CustomizePage';
-import AccountPage from './pages/AccountPage';
-import SupportPage from './pages/SupportPage';
 
 function App() {
   return (
@@ -24,13 +17,6 @@ function App() {
         {/* Dashboard */}
         <Route path="/dashboard" element={<DashboardLayout />}>
           <Route index element={<DashboardPage />} />
-          <Route path="campaigns" element={<CampaignsPage />} />
-          <Route path="configure-pix" element={<ConfigurePixPage />} />
-          <Route path="social-media" element={<SocialMediaPage />} />
-          <Route path="analytics" element={<AnalyticsPage />} />
-          <Route path="customize" element={<CustomizePage />} />
-          <Route path="account" element={<AccountPage />} />
-          <Route path="support" element={<SupportPage />} />
         </Route>
       </Routes>
     </Router>
