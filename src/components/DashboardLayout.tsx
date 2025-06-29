57d52577b8d1977b930b5ae7f8547362aaae952b
gilson954 <gilsonguigui0@gmail.com>
2025-06-29 03:18:33 -0300

    Updated DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 
 const DashboardLayout = () => {
   return (
-    <div className="min-h-screen bg-gray-950 flex">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-950 flex transition-colors duration-300">
       <Sidebar />
       <div className="flex-1">
         <Outlet />
