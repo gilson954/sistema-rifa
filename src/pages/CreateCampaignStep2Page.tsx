9f8576be28f109db8b422a8ddf92240814348202
Gilson <gilsonguigui0@gmail.com>
2025-07-23 13:04:55 -0300

    Updated CreateCampaignStep2Page.tsx

--- src/pages/CreateCampaignStep2Page.tsx
+++ src/pages/CreateCampaignStep2Page.tsx
@@ -656,29 +656,6 @@ const CreateCampaignStep2Page = () => {
             </button>
           </div>
 
-          {/* Quantidade de cotas - copied from CreateCampaignStep1Page */}
-          <div>
-            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
-              Quantidade de cotas *
-            </label>
-            <div className="relative">
-              <select
-                value={selectedQuotaQuantity}
-                onChange={handleQuotaQuantityChange}
-                className="w-full appearance-none px-4 py-3 border rounded-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-green-500 transition-colors duration-200 border-gray-300 dark:border-gray-600"
-                required
-              >
-                <option value="">Escolha uma opção</option>
-                {ticketQuantityOptions.map((option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-              <ChevronDown className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none" />
-            </div>
-          </div>
-
           {/* Imagens */}
           <div>
             <div className="mb-4">
