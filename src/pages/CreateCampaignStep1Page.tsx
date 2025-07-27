c4a26045881ade46f2d414e470fcd5de57aaab40
Gilson <gilsonguigui0@gmail.com>
2025-07-27 04:33:43 -0300

    Corrigir import do useEffect no CreateCampaignStep1Page

--- src/pages/CreateCampaignStep1Page.tsx
+++ src/pages/CreateCampaignStep1Page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ArrowRight, ChevronDown } from 'lucide-react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useCampaigns, useCampaign } from '../hooks/useCampaigns';
