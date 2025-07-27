ef87e2ad624f5566fed45741763ea5d94eeaf83b
Gilson <gilsonguigui0@gmail.com>
2025-07-27 01:47:41 -0300

    Updated DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -413,7 +413,7 @@ const DashboardPage = () => {
                     </div>
                   </div>
                 ))}
-                : (
+                ) : (
                   <div className="text-center py-12">
                     <div className="w-16 h-16 bg-gray-100 dark:bg-gray-800 rounded-full flex items-center justify-center mx-auto mb-4">
                       <Search className="h-8 w-8 text-gray-400" />
