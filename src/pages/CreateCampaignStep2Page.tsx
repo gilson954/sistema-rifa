d872348c1fd4be0db430caa40ded2ff629937a1a
Gilson <gilsonguigui0@gmail.com>
2025-08-18 02:32:23 -0300

    Updated CreateCampaignStep2Page.tsx

--- src/pages/CreateCampaignStep2Page.tsx
+++ src/pages/CreateCampaignStep2Page.tsx
@@ -1193,7 +1193,7 @@ const CreateCampaignStep2Page = () => {
                 <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white"></div>
               ) : (
                 <>
-                  <span>Continuar</span>
+                  <span>Finalizar</span>
                   <ArrowRight className="h-5 w-5" />
                 </>
               )}
