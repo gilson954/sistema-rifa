255870eb4bbf89215937238d4c6bda978cbed1cc
Gilson <gilsonguigui0@gmail.com>
2025-08-18 14:16:26 -0300

    Updated CreateCampaignStep2Page.tsx

--- src/pages/CreateCampaignStep2Page.tsx
+++ src/pages/CreateCampaignStep2Page.tsx
@@ -876,6 +876,10 @@ const CreateCampaignStep2Page = () => {
             </div>
           </div>
 
+                </div>
+                <p className="text-sm text-gray-600 dark:text-gray-400">
+                  O cliente escolhe os números que deseja comprar
+                </p>
               </div>
             </div>
           </div>
@@ -1146,7 +1150,6 @@ const CreateCampaignStep2Page = () => {
             </button>
           </div>
         </form>
-      </div>
 
         {/* Modals */}
         <PromotionModal
