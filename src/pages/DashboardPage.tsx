a1bbaf3d20582e5b31e47a41c1f4ffadf0ab380c
Gilson <gilsonguigui0@gmail.com>
2025-08-23 22:44:46 -0300

    Updated DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -372,7 +372,7 @@ const DashboardPage = () => {
                     </div>
                   </div>
 
-                  {campaign.status === 'draft' &&
+                  {campaign.status === 'draft' && (
                     <div className="mt-3 p-3 bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg">
                       <div className="flex items-start space-x-2">
                         <AlertTriangle className="h-4 w-4 text-yellow-600 dark:text-yellow-400 mt-0.5 flex-shrink-0" />
@@ -387,7 +387,7 @@ const DashboardPage = () => {
                         </div>
                       </div>
                     </div>
-                  }
+                  )}
                 ))}
               </div>
             )}
