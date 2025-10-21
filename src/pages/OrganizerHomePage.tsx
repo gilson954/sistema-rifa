3ec04c4ac666c4b4a25a2b8c8232a2f4544ccb7f
Gilson <gilsonguigui0@gmail.com>
2025-10-21 02:27:30 -0300

    Updated OrganizerHomePage.tsx

--- src/pages/OrganizerHomePage.tsx
+++ src/pages/OrganizerHomePage.tsx
@@ -220,7 +220,6 @@ const OrganizerHomePage: React.FC = () => {
   const handlePageChange = (page: number) => {
     if (page >= 1 && page <= totalPages) {
       setCurrentPage(page);
-      window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   };
 
