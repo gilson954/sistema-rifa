e33ce4d00a426549b8f90aaeee832b86933b2c23
Gilson <gilsonguigui0@gmail.com>
2025-11-08 18:28:02 -0300

    Updated CampaignHeader.tsx

--- src/components/CampaignHeader.tsx
+++ src/components/CampaignHeader.tsx
@@ -66,7 +66,7 @@ const CampaignHeader: React.FC<CampaignHeaderProps> = ({
         text: 'text-white',
         textSecondary: 'text-gray-400',
         cardBg: 'bg-[#2C2C2C]',
-        border: 'border-[#1f1f1f]'
+        border: 'border-[#153cbd]'
       };
       default:
         return {
