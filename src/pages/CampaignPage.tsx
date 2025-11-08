cb6863babbb364d6b22dbc64f480a20732baef42
Gilson <gilsonguigui0@gmail.com>
2025-11-08 14:11:30 -0300

    Updated CampaignPage.tsx

--- src/pages/CampaignPage.tsx
+++ src/pages/CampaignPage.tsx
@@ -859,7 +859,7 @@ const CampaignPage = () => {
           text: 'text-white',
           textSecondary: 'text-gray-400',
           cardBg: 'bg-[#2C2C2C]',
-          border: 'border-gray-700',
+          border: 'border-gray-900',
           rifaquiText: 'text-white'
         };
       default:
