e01e0391e0e28a9db9e1bff7cfadb2436a648ff9
Gilson <gilsonguigui0@gmail.com>
2025-11-08 02:29:12 -0300

    Updated CampaignFooter.tsx

--- src/components/CampaignFooter.tsx
+++ src/components/CampaignFooter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { useTheme } from '../contexts/ThemeContext';
+import { useTheme } from '../../contexts/ThemeContext';
 
 interface CampaignFooterProps {
   campaignTheme?: string;
