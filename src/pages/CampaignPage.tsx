c5ee73a19fd0c7684f5316b443fa468afb710bb4
Gilson <gilsonguigui0@gmail.com>
2025-11-09 03:03:03 -0300

    Updated CampaignPage.tsx

--- src/pages/CampaignPage.tsx
+++ src/pages/CampaignPage.tsx
@@ -835,7 +835,7 @@ const CampaignPage = () => {
           border: 'border-slate-600',
           rifaquiText: 'text-white',
           calendarBg: '#141414',
-          calendarBorder: '#475569'
+          calendarBorder: '#c71212'
         };
       case 'escuro-preto':
         return {
