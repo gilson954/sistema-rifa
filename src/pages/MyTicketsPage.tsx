fc5e9d695aa124f59096e9cb9c2bbf6229540417
Gilson <gilsonguigui0@gmail.com>
2025-11-10 00:58:09 -0300

    Updated MyTicketsPage.tsx

--- src/pages/MyTicketsPage.tsx
+++ src/pages/MyTicketsPage.tsx
@@ -299,11 +299,11 @@ const MyTicketsPage = () => {
     if (campaignTheme === 'claro') {
       return isHover 
         ? 'shadow-[0_15px_45px_-10px_rgba(0,0,0,0.3),0_8px_22px_-6px_rgba(0,0,0,0.12)]'
-        : 'shadow-[0_8px_30px_-8px_rgba(0,0,0,0.2),0_4px_15px_-4px_rgba(0,0,0,0.12)]';
+        : 'shadow-[0_8px_30px_-8px_rgba(0,0,0,0.2),0_4px_15px_-4px_rgba(0,0,0,0.16)]';
     }
     return isHover
-      ? 'shadow-[0_15px_45px_-10px_rgba(0,0,0,0.7),0_8px_22px_-6px_rgba(0,0,0,0.5)]'
-      : 'shadow-[0_8px_30px_-8px_rgba(0,0,0,0.6),0_4px_15px_-4px_rgba(0,0,0,0.4)]';
+      ? 'shadow-[0_15px_45px_-10px_rgba(0,0,0,0.7),0_8px_22px_-6px_rgba(0,0,0,0.12)]'
+      : 'shadow-[0_8px_30px_-8px_rgba(0,0,0,0.6),0_4px_15px_-4px_rgba(0,0,0,0.12)]';
   };
 
   if (authLoading || loading) {
