f033518e96a8d1f11de28060d47cb55414488f0f
Gilson <gilsonguigui0@gmail.com>
2025-11-14 11:15:24 -0300

    Updated PaymentConfirmationPage.tsx

--- src/pages/PaymentConfirmationPage.tsx
+++ src/pages/PaymentConfirmationPage.tsx
@@ -113,7 +113,7 @@ const PaymentConfirmationPage = () => {
         cardBg: 'bg-[#2C2C2C]',
         border: 'border-[#1f1f1f]',
         inputBg: 'bg-[#2C2C2C]',
-        idBoxBg: 'bg-[#a34040]',
+        idBoxBg: 'bg-[#3C3C3C]',
         idBoxText: 'text-gray-200',
         stepBg: 'bg-green-500',
         stepText: 'text-white',
